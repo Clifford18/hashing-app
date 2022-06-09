d1b4777c0ce28a61a9b6cd5e9f057826d784e3af
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-09 15:53:15 +0300

    update HashingComponent.js file

--- src/components/HashingComponent.js
+++ src/components/HashingComponent.js
@@ -1,13 +1,52 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import './HashingStyles.css';
 
-export default function HashingComponent(){
+export default function HashingComponent() {
 
 	//initial states
-	const [algorithms] = useState(['sha1','sha256','sha384','sha512']);
+	const [algorithms] = useState(['sha1', 'sha256', 'sha384', 'sha512']);
 	let [text_input, setTextInput] = useState('');
 	let [file_input, setFileInput] = useState('');
 	let [algorithm, setAlgorithm] = useState('sha1');
-	let [output,setOutput] = useState('');
+	let [output, setOutput] = useState('');
+
+	//set up on change handlers
+	// For handling text input
+	const handleTextInput = async (e) => {
+	};
+
+  	// For handling file input
+	const handleFileInput = (e) => {
+	};
+
+	// For handling algorithm change
+	const handleAlgorithmChange = async (e) => {
+	};
+
+
+	//render the input fields
+	return (
+		<div className='hashing-container'>
+			<div className='hashing-content'>
+
+				// Start: Hashing form
+				<div className="hashing-form">
+					<h4 className="hashing-form-heading">Input</h4>
+					<form>
+						<div className="form-group">
+							<label htmlFor="text-input">Text</label>
+							<input type="text" className="form-control" id="text-input" placeholder='Write some text' value={text_input} onChange={handleTextInput} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="file-input">File Input</label>
+							<input type="file" className="form-control" id="file-input" onChange={handleFileInput} />
+						</div>
+					</form>
+				</div>
+				// End: Hashing form
+
+			</div>
+		</div>
+	);
 
 }
