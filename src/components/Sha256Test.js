c23daf51df935f0da6f5c7d41644bcb5651d7d6e
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-14 12:57:35 +0300

    update Sha256Test.js file

--- src/components/Sha256Test.js
+++ src/components/Sha256Test.js
@@ -1,12 +1,16 @@
 
 // const {sha256} = require ("crypto-hash");
 
+
 import {sha256} from "crypto-hash";
+
+console.log("Hello world!");
 
 let text_input = "Pa%%w0rd";
 
 let result = '';
 
-result = ( sha256(text_input));
+result = (await sha256(text_input));
 
 console.log(result)
+
