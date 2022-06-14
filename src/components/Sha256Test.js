d82d8fe75129aca4031516c863a3323d44c60de0
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-14 14:09:19 +0300

    update Sha256Test.js finalle

--- src/components/Sha256Test.js
+++ src/components/Sha256Test.js
@@ -6,11 +6,12 @@ import {sha256} from "crypto-hash";
 
 console.log("Hello world!");
 
-let text_input = "Pa%%w0rd";
+let text_input = "John Clifford";
 
 let result = '';
 
 result = (await sha256(text_input));
 
 console.log(result)
+console.log(result==="d2374dc98cbcf6a6ec374903b47b467224f4827b5736d1f15cb4a9030e16678a")
 
