20f7a38d90ec179f1177d120085c3433c27f9d33
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-14 14:10:16 +0300

    update BcryptTest.js finale

--- src/components/BcryptTest.js
+++ src/components/BcryptTest.js
@@ -4,12 +4,21 @@
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
-var password = "Fkdj^45ci@Jad";
+var password = "Clifford Karani";
 
-bcrypt.genSalt(saltRounds, function(err, salt) {
-	bcrypt.hash(password, salt, function(err, hash) {
-		// Store hash in database here
-		console.log("hash:",hash);
-	});
+// bcrypt.genSalt(saltRounds, function(err, salt) {
+// 	bcrypt.hash(password, salt, function(err, hash) {
+// 		// Store hash in database here
+// 		console.log("hash:",hash);
+// 	});
+// });
+
+bcrypt.hash(password, saltRounds, function(err, hash) {
+	// Store hash in database here
+	console.log("hash:",hash);
+
+	bcrypt.compare("Password",hash,function(err, result) {
+
+		console.log("result match is:",result);
+	})
 });
-
