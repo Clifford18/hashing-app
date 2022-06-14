899547914a43b159961c2d8648de573cd07887ed
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-14 13:41:02 +0300

    update Base64EncodeTest.js file

---
+++ src/components/Base64EncodeTest.js
@@ -0,0 +1,12 @@
+
+// const {sha256} = require ("crypto-hash");
+
+
+var decodedStringBtoA = 'Hello World!';
+
+// Encode the String
+var encodedStringBtoA = btoa(decodedStringBtoA);
+
+console.log("Encoded:",encodedStringBtoA);
+
+
