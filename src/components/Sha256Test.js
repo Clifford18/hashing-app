9669ce191af0abed0ed8b2a5053f26d1c784764d
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-15 12:18:02 +0300

    update Sha256Test.js file

--- src/components/Sha256Test.js
+++ src/components/Sha256Test.js
@@ -3,6 +3,7 @@
 
 
 import {sha256} from "crypto-hash";
+import {ComputeAuthHashComponent} from "./ComputePassword.js";
 
 console.log("Hello world!");
 
@@ -14,4 +15,6 @@ result = (await sha256(text_input));
 
 console.log(result)
 console.log(result==="d2374dc98cbcf6a6ec374903b47b467224f4827b5736d1f15cb4a9030e16678a")
+
+console.log("Auth hash:", await ComputeAuthHashComponent("johnkarani@skyworld.co.ke","Sky@2022!"))
 
