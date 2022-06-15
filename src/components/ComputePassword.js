333c93016b46d97448a6e3a26e08eff0558c9b8a
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-15 12:17:43 +0300

    update ComputePassword.js file

--- src/components/ComputePassword.js
+++ src/components/ComputePassword.js
@@ -1,14 +1,14 @@
 import {sha256} from "crypto-hash";
 import bcrypt from "bcryptjs";
 
-let password = "Sky@2022!";
-let username ="johnkarani@skyworld.co.ke::";
+let AuthPassword = "Sky@2022!";
+let AuthUsername ="johnkarani@skyworld.co.ke";
 const saltRounds = 10;
 
-const computeAuthHash = async (username, password) => {
-	// base64Encode(bCrypt(username::lower(SHA256(password)))
+export const ComputeAuthHashComponent = async (username, password) => {
+	// base64Encode(bCrypt(username::lower(SHA256(AuthPassword)))
 	return btoa(bcrypt.hashSync(`${username}::${(await sha256(password))}`, saltRounds))
 }
 
-console.log("Auth hash:", await computeAuthHash(username, password))
+console.log("Auth hash:", await ComputeAuthHashComponent(AuthUsername, AuthPassword))
 
